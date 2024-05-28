4348a89aea384e4b220b6e0e19ae9f6eb98f9397
AhmetTanerKahraman <kahramanahmettaner@gmail.com>
2024-05-28 22:12:00 +0200

    feat: add example graph JSON file and integrate it in Home component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,14 +1,15 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router, RouterLink } from '@angular/router';
 import { GraphService } from '../services/graph.service';
+import * as graphData from '../../assets/graphData.json';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterLink, CommonModule, ReactiveFormsModule],
+  imports: [RouterLink, CommonModule, ReactiveFormsModule, FormsModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
@@ -17,6 +18,7 @@ export class HomeComponent {
   form: FormGroup;
   options = ['Binärer Suchbaum', 'Graph'];
   showCheckbox = false;
+  predesigned_examples = graphData.examples;
 
   constructor(
     private router: Router,
@@ -71,4 +73,31 @@ export class HomeComponent {
     this.showCheckbox = option === 'Graph'; 
   }
 
+  choosePredesigned(index: number) {
+
+          // For now, Binary Search Tree is not implemented suitable for this
+          // Graph:
+          if (this.predesigned_examples[index].data.structureType === "graph") {
+            // Reset Graph State
+            this.graphService.resetGraph();
+    
+            // Set Graph Configuration
+            this.graphService.configureGraph({
+              nodes: {
+                visited: false,
+                weight: this.predesigned_examples[index].data.configuration.nodes.weight
+              },
+              edges: {
+                directed: this.predesigned_examples[index].data.configuration.edges.directed,
+                weight: this.predesigned_examples[index].data.configuration.edges.weight
+              }
+            })
+            
+            const graphJSON = JSON.stringify(this.predesigned_examples[index].data, null, 2);
+            this.graphService.graphFromJSON(graphJSON);
+    
+            this.navigateToRoute('/graph')
+          }
+
+  }
 }
