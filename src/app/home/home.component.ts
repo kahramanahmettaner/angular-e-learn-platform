f31de57b950084bc494015e39b1886ebec4895d4
AhmetTanerKahraman <kahramanahmettaner@gmail.com>
2024-06-07 21:51:26 +0200

    feat: add AssignmentService with initial implementation
    - Added AssignmentService to manage assignments
    - Implemented methods to fetch assignments, create new assignments, and retrieve an assignment by ID
    - Utilized BehaviorSubject to provide observable updates
    - Initial implementation uses dummy data until backend integration is complete
    - Deleted graphData.json as it contained dummy data for example graph assignments, which is not compatible with the current code
    - Adjusted the Home component to remove dependencies on graphData.json

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -4,7 +4,6 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router, RouterLink } from '@angular/router';
 import { GraphService } from '../services/graph.service';
-import * as graphData from '../../assets/graphData.json';
 
 @Component({
   selector: 'app-home',
@@ -18,7 +17,6 @@ export class HomeComponent {
   form: FormGroup;
   options = ['Binärer Suchbaum', 'Graph'];
   showCheckbox = false;
-  predesigned_examples = graphData.examples;
 
   constructor(
     private router: Router,
@@ -71,33 +69,5 @@ export class HomeComponent {
 
     // Show checkboxes only if graph structure is selected
     this.showCheckbox = option === 'Graph'; 
-  }
-
-  choosePredesigned(index: number) {
-
-          // For now, Binary Search Tree is not implemented suitable for this
-          // Graph:
-          if (this.predesigned_examples[index].data.structureType === "graph") {
-            // Reset Graph State
-            this.graphService.resetGraph();
-    
-            // Set Graph Configuration
-            this.graphService.configureGraph({
-              nodes: {
-                visited: false,
-                weight: this.predesigned_examples[index].data.configuration.nodes.weight
-              },
-              edges: {
-                directed: this.predesigned_examples[index].data.configuration.edges.directed,
-                weight: this.predesigned_examples[index].data.configuration.edges.weight
-              }
-            })
-            
-            const graphJSON = JSON.stringify(this.predesigned_examples[index].data, null, 2);
-            this.graphService.graphFromJSON(graphJSON);
-    
-            this.navigateToRoute('/graph')
-          }
-
   }
 }
---
+++ src/app/services/assignment.service.ts
@@ -0,0 +1,84 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable, map } from 'rxjs';
+import IAssignment from '../models/Assignment.interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AssignmentService {
+
+  private assignments$ = new BehaviorSubject<IAssignment[]>([]);
+  
+  constructor() {
+    this.init();  
+  }
+
+  public init(): void {
+    // TODO: fetch assigments from backend and update assignments$
+    
+    // for now, use dummy data instead
+    const dummy_assignments: IAssignment[] = this.dummy_data();
+    this.assignments$.next(dummy_assignments);
+  }
+
+  public getAssignments(): Observable<IAssignment[]> {
+    return this.assignments$;
+  }
+
+  createAssignment(newAssignment: IAssignment) {
+    // TODO:  Send a POST request to the backend to create a new assignment
+    
+    // for now, add new assignment to the list
+    this.assignments$.next([ ...this.assignments$.getValue(), newAssignment])
+  }
+
+  public getAssignmentById(id: number): Observable<IAssignment | undefined> {
+    return this.assignments$.pipe(
+      map((assignments) => assignments.find((assignment) => assignment.id === id))
+    );
+  }
+
+  // TODO: delete this function
+  public dummy_data(): IAssignment[] {
+    return [
+      { 
+        "id": 1,
+        "title": "Aufgabe 1",
+        "text": "Text für Aufgabe 1",
+        "stepsEnabled": false,
+        "dataStructure": "graph",
+        "graphConfiguration": {
+          "initialNodeData": [],
+          "initialEdgeData": [],
+          "nodeConfiguration": {
+            "weight": false,
+            "visited": false
+          },
+          "edgeConfiguration": {
+            "directed": true,
+            "weight": true
+          }
+        }
+      },
+      {
+        "id": 2,
+        "title": "Aufgabe 2",
+        "text": "Text für Aufgabe 2",
+        "stepsEnabled": false,
+        "dataStructure": "graph",
+        "graphConfiguration": {
+          "initialNodeData": [],
+          "initialEdgeData": [],
+          "nodeConfiguration": {
+            "weight": false,
+            "visited": false
+          },
+          "edgeConfiguration": {
+            "directed": false,
+            "weight": true
+          }
+        }
+      }
+    ]
+  }
+}
