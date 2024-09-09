417af3a2f7c4ac8171b5cc346d76244237cebc9c
AhmetTanerKahraman <kahramanahmettaner@gmail.com>
2024-09-09 12:41:01 +0200

    feat(backend): enhance AssignmentsService with database integration and input validation

--- backend/src/assignments/assignments.service.ts
+++ backend/src/assignments/assignments.service.ts
@@ -7,22 +7,91 @@ export class AssignmentsService {
   constructor(private readonly databaseService: DatabaseService){}
 
   async create(createAssignmentDto: Prisma.AssignmentCreateInput) {
-    return 'This action adds a new assignment';
+
+    const {
+      title, text, dataStructure, stepsEnabled, 
+      initialStructure, expectedStructure, graphConfiguration
+    } = createAssignmentDto;
+
+    if (title === undefined || typeof title !== 'string') {
+      return "Die Aufgabe muss einen gültigen Titel haben."
+    }
+
+    if (text === undefined || typeof text !== 'string') {
+      return "Die Aufgabe muss einen gültigen text Field haben."
+    }
+
+    // If stepsEnabled is not valid, assign false as default
+    if (stepsEnabled === undefined || typeof stepsEnabled !== 'boolean') {
+      createAssignmentDto.stepsEnabled = false;
+    }
+
+    // If dataStructure is not valid, do not continue
+    if (dataStructure !== 'graph' && dataStructure !== 'tree') {
+      return "Datenstruktur muss entweder 'graph' oder 'tree' sein."
+    }
+
+    // TODO: check type
+    // If initialStructure is not valid, assign default value
+    if (!initialStructure) {
+      if (dataStructure === 'tree') {
+        createAssignmentDto.initialStructure = null;
+      }
+      else {
+        createAssignmentDto.initialStructure = { nodes: [], edges: []};
+      }
+    }
+
+    // TODO: check type
+    // If expectedStructure is not valid, assign default value
+    if (!expectedStructure) {
+      if (dataStructure === 'tree') {
+        createAssignmentDto.expectedStructure = null;
+      }
+      else {
+        createAssignmentDto.expectedStructure = { nodes: [], edges: []};
+      }
+    }
+
+    // Check if graphConfiguration is valid
+    if (dataStructure === 'tree') {
+      createAssignmentDto.graphConfiguration = null;
+    }
+    else {
+      if (!graphConfiguration) {
+        return 'Graph Konfiguration ist nicht gültig.'
+      }
+    }
+
+    // Create the assignment
+    // Do not allow id input from client
+    return this.databaseService.assignment.create({
+      data: { ...createAssignmentDto, id: undefined }
+    })
   }
 
   async findAll() {
-    return `This action returns all assignments`;
+    return this.databaseService.assignment.findMany();
   }
 
   async findOne(id: number) {
-    return `This action returns a #${id} assignment`;
+    const assignment = this.databaseService.assignment.findUnique({
+      where: { id }
+    });
+
+    if (assignment) {
+      return assignment;
+    }
+    return `Keine Aufgabe mit id=${id} gefunden.`
   }
 
   async update(id: number, updateAssignmentDto: Prisma.AssignmentUpdateInput) {
-    return `This action updates a #${id} assignment`;
+    return `This action updates a #${id} assignment: ${updateAssignmentDto}`;
   }
 
   async remove(id: number) {
-    return `This action removes a #${id} assignment`;
+    return this.databaseService.assignment.delete({
+      where: { id }
+    })
   }
 }
